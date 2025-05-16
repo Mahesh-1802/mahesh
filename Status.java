2d2b5ada76fb34df286090fdf56f78f26cd340d0
Mahesh-1802 <maheshbolla02@gmail.com>
2025-05-16 14:48:43 +0530

    new java files

---
+++ Status.java
@@ -0,0 +1,13 @@
+class Status
+{
+	public static void main(String[]args)
+		{
+			int age = 45;
+			if(age<18)
+				System.out.println("minor");
+			else if(age>=18)
+				System.out.println("major");
+			else if(age>=60)
+				System.out.println("Senior Citizen");
+		}
+}
---
+++ Switch1.java
@@ -0,0 +1,31 @@
+class Switch1 
+{
+	public static void main(String[]args)
+	{
+		int a = 'z';
+		switch(a)
+		{
+			case 1:
+				System.out.println("sunday");
+			break;
+			case 2:
+				System.out.println("monday");
+			break;
+			case 3:
+				System.out.println("tuesday");
+			break;
+			case 4:
+				System.out.println("wednesday");
+			break;
+			case 122:
+				System.out.println("thurday");
+			break;
+			case 6:
+				System.out.println("friday");
+			break;
+			case 7:
+				System.out.println("saturday");
+			break;
+		}
+	}
+}
